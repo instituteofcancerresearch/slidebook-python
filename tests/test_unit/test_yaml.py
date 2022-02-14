f348c1894132ce168eefca0004718f0075982ed8
Adam Tyson <code@adamltyson.com>
2022-02-14 16:57:51 +0000

    Add yaml test

--- tests/test_unit/test_yaml.py
+++ tests/test_unit/test_yaml.py
@@ -7,5 +7,10 @@ test_yaml = data_dir / "test.yaml"
 
 def test_open_yaml():
     yaml_contents = open_yaml(test_yaml)
-    assert False
-
+    assert len(yaml_contents) == 4
+    assert yaml_contents["a"] == 1
+    assert yaml_contents["b"] == 2
+    assert yaml_contents["c"] == "three"
+    assert len(yaml_contents["TestClass"]) == 2
+    assert yaml_contents["TestClass"]["ClassName"] == "TestClass"
+    assert yaml_contents["TestClass"]["num"] == 200

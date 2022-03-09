eb33ae350521048246a06d7dffb5138bc8363c48
Adam Tyson <code@adamltyson.com>
2022-03-09 14:14:41 +0000

    fix test

--- tests/test_integration/test_load.py
+++ tests/test_integration/test_load.py
@@ -21,16 +21,4 @@ def test_load_sldy():
     assert sld.images[0].data["ch_0"][0].shape == (4, 1024, 1024)
     assert sld.images[0].data["ch_0"][0][2, 10, 10] == 152
 
-    assert len(sld.images[0].metadata.annotation_record) == 7
-    assert len(sld.images[0].metadata.aux_data) == 11
-    assert len(sld.images[0].metadata.channel_record) == 2
-    assert len(sld.images[0].metadata.elapsed_times) == 1
-    assert len(sld.images[0].metadata.image_record) == 2
-    assert len(sld.images[0].metadata.mask_record) == 1
-    assert len(sld.images[0].metadata.sa_position_data) == 2
-    assert len(sld.images[0].metadata.stage_position_data) == 8
-
     assert sld.images[0].name == "StageScan1643299934"
-
-    assert len(sld.metadata.sldy_contents) == 2
-    assert len(sld.metadata.sldy_contents["StartClass"]) == 19
